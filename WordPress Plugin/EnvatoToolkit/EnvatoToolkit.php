77243fb1646b4c59e40897df664548cb132be23d
Kestutis Matuliauskas <kestutis.itsolutions@gmail.com>
2017-06-20 00:44:05 +0300

    Description updated

--- WordPress Plugin/EnvatoToolkit/EnvatoToolkit.php
+++ WordPress Plugin/EnvatoToolkit/EnvatoToolkit.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Envato Toolkit
  * Plugin URI: http://EnvatoToolkit.com
- * Description: Add custom notes before or after the comment form.
+ * Description: It is a 3 files library + Visual UI, to validate the purchase codes of your customers, get details about specific Envato user (country, city, total followers, total sales, avatar), get his license purchase and support expiration dates, license type he bought, check for updates of purchased plugins and themes and get the download links for them. Plus - this library has Envato Item Id search feature by providing plugin's or theme's name and author.
  * Version: 1.0
  * Author: KestutisIT
  * Author URI: https://profiles.wordpress.org/KestutisIT
